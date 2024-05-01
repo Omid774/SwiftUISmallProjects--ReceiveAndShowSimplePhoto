7e00693c34caf1e5da451d318e84093f48d61d35
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-05-01 10:09:24 +0330

    Initial Commit

---
+++ Receive&ShowSimplePhoto/Receive_ShowSimplePhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Receive_ShowSimplePhotoApp.swift
+//  Receive&ShowSimplePhoto
+//
+//  Created by Omid Heydarzadeh on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Receive_ShowSimplePhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
